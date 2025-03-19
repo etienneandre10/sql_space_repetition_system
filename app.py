9be9cfdfb402f106a48af1ba98d14c7965a63856
Etienne <etienne.andre9@gmail.com>
2025-03-19 16:18:43 +0000

    modification Etienne

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ with st.sidebar:
 data={"a":[1,2,3], "b":[4,5,6]}
 df=pd.DataFrame(data)
 
-tab1, tab2, tab3=st.tabs(["Onglet1","Onglet2","Onglet3"])
 
 with tab1:
     sql_query=st.text_area(label="entrez votre input")
