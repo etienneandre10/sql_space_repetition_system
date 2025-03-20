38f2d07e7bf5c0d7c7e6ee71745ab0cbbb49f50c
Etienne <etienne.andre9@gmail.com>
2025-03-20 11:12:19 +0000

    correction app

--- app.py
+++ app.py
@@ -1,77 +1,104 @@
-import io
+# pylint: disable=missing-module-docstring
 
+import os
+import logging
+import duckdb
 import streamlit as st
-import pandas as pd
-import duckdb as db
-
-st.write("SQL space repetition system")
+from datetime import date, timedelta
 
 
-csv='''
-beverage, price
-orange juice, 2.5
-expresso, 2
-tea, 2
-'''
+if "data" not in os.listdir():
+    print("creating folder data")
+    logging.error(os.listdir())
+    logging.error("creating folder data")
+    os.mkdir("data")
 
-beverages=pd.read_csv(io.StringIO(csv))
+if "exercises_sql_tables.duckdb" not in os.listdir("data"):
+    exec(open("init_db.py").read())
+    # subprocess.run(["python", "init_db.py"])
 
-csv2='''
-food, price
-chocolatine,1
-croissant,2
-gateau,3
-'''
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-food=pd.read_csv(io.StringIO(csv2))
 
-answer_str='''
-SELECT * FROM beverages
-CROSS JOIN food
-'''
-
-solution_df=db.sql(answer_str)
-
-with st.sidebar:
-    option = st.selectbox(
-        "What would you like to review ?",
-        ("Join", "Groupby", "Window function"),
-        index=None,
-        placeholder="Select a theme",
-    )
-    st.write("You selected :", option)
-
-st.header('enter your code:')
-query=st.text_area(label="votre code sql ici", key="user input")
-
-if query:
-    result=db.sql(query)
+def check_users_solution(user_query: str) -> None:
+    """
+    Checks that user SQL query is correct by:
+    1: checking the columns
+    2: checking the values
+    :param user_query: a string containing the query inserted by the user
+    """
+    result = con.execute(user_query).df()
     st.dataframe(result)
-
     try:
-        result=result[solution_df.columns]
+        result = result[solution_df.columns]
         st.dataframe(result.compare(solution_df))
-
+        if result.compare(solution_df).shape == (0, 0):
+            st.write("Correct !")
+            st.balloons()
     except KeyError as e:
         st.write("Some columns are missing")
-
-    n_lines_differences=result.shape[0] - solution_df.shape[0]
-    if n_lines_differences !=0:
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
         st.write(
-            f"result has a {n_lines_differences} lines difference with the solution_df"
+            f"result has a {n_lines_difference} lines difference with the solution_df"
         )
 
 
+with st.sidebar:
+    available_themes_df = con.execute("SELECT DISTINCT theme FROM memory_state").df()
+    theme = st.selectbox(
+        "What would you like to review?",
+        available_themes_df["theme"].unique(),
+        index=None,
+        placeholder="Select a theme...",
+    )
+    if theme:
+        st.write(f"You selected {theme}")
+        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+    else:
+        select_exercise_query = f"SELECT * FROM memory_state"
 
-tab2, tab3=st.tabs(["Tables","Solutions"])
+    exercise = (
+        con.execute(select_exercise_query)
+        .df()
+        .sort_values("last_reviewed")
+        .reset_index(drop=True)
+    )
+    st.write(exercise)
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
 
+    solution_df = con.execute(answer).df()
+
+st.header("enter your code:")
+form = st.form("my_form")
+query = form.text_area(label="votre code SQL ici", key="user_input")
+form.form_submit_button("Submit")
+
+if query:
+    check_users_solution(query)
+
+for n_days in [2, 7, 21]:
+    if st.button(f"Revoir dans {n_days} jours"):
+        next_review = date.today() + timedelta(days=n_days)
+        con.execute(
+            f"UPDATE memory_state SET last_reviewed = '{next_review}' WHERE exercise_name = '{exercise_name}'"
+        )
+        st.rerun()
+
+if st.button("Reset"):
+    con.execute(f"UPDATE memory_state SET last_reviewed = '1970-01-01'")
+    st.rerun()
+
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food")
-    st.dataframe(food)
-    st.write("expected:")
-    st.dataframe(solution_df)
+    exercise_tables = exercise.loc[0, "tables"]
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
 
 with tab3:
-    st.write(answer_str)
+    st.write(answer)
